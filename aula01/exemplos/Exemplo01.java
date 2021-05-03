8b7268c20dc9a238b53fcc11c9963b465f9b62bc
Julia Zuin <zuinjulia@hotmail.com>
2021-05-03 12:40:25 -0300

    primeiro commit aula 1

---
+++ aula01/exemplos/Exemplo01.java
@@ -0,0 +1,11 @@
+package exemplos;
+
+/**
+ * Exemplo01
+ */
+public class Exemplo01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");       
+        
+    }
+}
---
+++ aula01/exercicios/Exercicio01.java
@@ -0,0 +1,10 @@
+package exercicios;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        System.out.println("15+7=" + (15 + 7)); // alt + shift + seta baixo (duplica linha)
+        System.out.println("15-7=" + (15 - 7));
+        System.out.println("15*7=" + (15 * 7));
+        System.out.println("15/7=" + (15 / 7));
+    }
+}
