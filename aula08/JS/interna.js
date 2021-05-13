1a3fa21116a5eb3087f92a5cf6cda2f551873b99
Julia Zuin <zuinjulia@hotmail.com>
2021-05-13 16:57:16 -0300

    primeiros passos web

--- aula08/JS/interna.js
+++ aula08/JS/interna.js
@@ -15,3 +15,41 @@ function logout() {
     localStorage.removeItem("userLogged")
     window.location = "index.html"
 }
+
+function listarUsuarios() {
+    fetch("http://localhost:8080/user/all")
+        .then(res => tratarRetorno(res))
+}
+
+function tratarRetorno(resposta) {
+    resposta.status == 200 ? resposta.json().then(res => getListaUser(res)) : document.getElementById("msgErro").innerHTML = "Falha ao carregar"
+}
+
+function getListaUser(res) {
+    let tableHeader = "<thead> <tr> <th>Nome</th> <th>Email</th> <th>CPF</th> </tr> </thead>"
+
+    let tableMap = res.map(function (item) {
+        tabela = `<tr>
+            <td> ${item.nome} </td>
+            <td> ${item.email} </td>
+            <td> ${item.cpf} </td>
+        </tr>`
+        return tabela
+    })
+    document.getElementById("headTable").innerHTML = tableHeader;
+    document.getElementById("bodyTabela").innerHTML = tableMap;
+
+    montarSelect(res);
+}
+
+function montarSelect(listarUsuarios) {
+    let selecao = "<select>"
+    let selectMap = listarUsuarios.map(function (item) {
+        tabela = `<option> ${item.nome} </option>`
+        return tabela
+    })
+
+
+    document.getElementById("selecao").innerHTML = selecao + selectMap + "</select>";
+
+}
